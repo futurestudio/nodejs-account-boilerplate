6fec204442ad9c826615a5b2018fd93d986a11b1
Norman Peitek <norman.peitek@gmail.com>
2014-06-11 16:33:16 -0500

    added express4 depedencies

--- server/app.js
+++ server/app.js
@@ -3,24 +3,36 @@
  * Module dependencies.
  */
 
+var express = require('express');
+
+// express 4 dependencies
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var session = require('express-session');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var errorhandler = require('error-handler');
+
+// accounts
+var passport = require('./modules/auth');
+
+// api
+var api = require('./routes/api');
+var UserAPI = require('./api/user-api');
+
+// website controller
+var routes = require('./controller/');
+var user = require('./controller/user');
+
+var path = require('path');
+
 module.exports = function (env) {
 
     if (env !== undefined) {
         process.env.NODE_ENV = env;
     }
 
-    var express = require('express');
-    var passport = require('./modules/auth');
-
-    // api
-    var api = require('./routes/api');
-    var UserAPI = require('./api/user-api');
-
-    // website controller
-    var routes = require('./controller/');
-    var user = require('./controller/user');
-
-    var path = require('path');
     var app = express();
 
     // internationalization
@@ -32,40 +44,43 @@ module.exports = function (env) {
         directory: __dirname + '/locales'
     });
 
-    // get database instance
-    // depending on the environment, use a different database
-    var db = require('./settings/db')(app.get('env'));
-    var MongoStore = require('connect-mongo')(express);
-
     // all environments
     app.set('port', process.env.PORT || 3000);
     app.set('views', path.join(__dirname, '../public/views'));
     app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.cookieParser());
-    app.use(express.session({
+
+    app.use(morgan('dev'));
+
+    // get database instance
+    // depending on the environment, use a different database
+    var db = require('./settings/db')(app.get('env'));
+    var MongoStore = require('connect-mongo')(session);
+
+    //app.use(favicon());
+    app.use(cookieParser());
+    app.use(session({
         secret: 'keyboard cat two', // that's a secret phrase to encrypt the session key
         store: new MongoStore({
             mongoose_connection: db
         })
     }));
+
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.methodOverride());
+
+    app.use(bodyParser());
+    app.use(methodOverride());
+
     app.use(function (req, res, next) {
         res.set('X-Powered-By', 'nodejs-account-boilerplate');
         next();
     });
     app.use(i18n.init);
-    app.use(app.router);
     app.use(express.static(path.join(__dirname, '../public/static')));
 
     // development only
     if ('development' == app.get('env')) {
-      app.use(express.errorHandler());
+      //app.use(errorhandler());
     }
 
     // include this block to force HTTPS on heroku.com deployments
