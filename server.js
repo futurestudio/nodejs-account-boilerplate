81d76035253e3cc8321a5cb859b3d078da747815
marcuspoehls <marcus.poehls@gmail.com>
2014-08-25 17:15:15 +0200

    update to express 4.8.5

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
  * Created by npeitek on 11/7/13.
  */
 
-var http = require('http'),
-    app = require('./server/app')();
+var app = require('./server/app')();
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
